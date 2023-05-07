1e277eabdcf340e4f93c311ebcc5b96f7a244a23
VolkerPerm <127930451+VolkerPerm@users.noreply.github.com>
2023-05-07 11:01:01 +0500

    Diplom_project
    Первый раз тут что-то делаю. Надеюсь всё получится)

---
+++ core.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+from pprint import pprint
+
+import vk_api
+
+from config import acces_token
+from data_store import checked
+
+
+class VkTools():
+    def __init__(self, acces_token):
+        self.api = vk_api.VkApi(token=acces_token)
+
+    def get_profile_info(self, user_id):
+
+        info, = self.api.method('users.get',
+                                {'user_id': user_id,
+                                 'fields': 'city, bdate, sex, relation, home_town'
+                                 }
+                                )
+        user_info = {'name': info['first_name'] + ' ' + info['last_name'],
+                     'id': info['id'],
+                     'bdate': info['bdate'] if 'bdate' in info else None,
+                     'home_town': info['home_town'],
+                     'sex': info['sex'],
+                     'city': info['city']['id']
+                    }
+        return user_info
+
+    def search_users(self, params):
+
+        sex = 1 if params['sex'] == 2 else 2
+        city = params['city']
+        curent_year = datetime.now().year
+        user_year = int(params['bdate'].split('.')[2])
+        age = curent_year - user_year
+        age_from = age - 5
+        age_to = age + 5
+
+
+
+        users = self.api.method('users.search',
+                                {'count': 10,
+                                 'offset': 0,
+                                 'age_from': age_from,
+                                 'age_to': age_to,
+                                 'sex': sex,
+                                 'city': city,
+                                 'status': 6,
+                                 'id_closed': False
+                                 }
+                                )
+        try:
+            users = users['items']
+        except KeyError:
+            return []
+
+        res = []
+
+        for user in users:
+            if user['is_closed'] == False:
+                if checked(params['id'], user['id']) == True:
+                    res.append({'id': user['id'],
+                                'name': user['first_name'] + ' ' + user['last_name']
+                                })
+
+        return res
+
+    def get_photos(self, user_id):
+        photos = self.api.method('photos.get',
+                                {'user_id': user_id,
+                                'album_id': 'profile',
+                                'extended': 1
+                                }
+                                )
+
+        try:
+            photos = photos['items']
+        except KeyError:
+            return []
+
+        res = []
+
+        for photo in photos:
+            res.append({'owner_id': photo['owner_id'],
+                        'id': photo['id'],
+                        'likes': photo['likes']['count'],
+                        'comments': photo['comments']['count']
+                        }
+                        )
+
+        res.sort(key=lambda x: x['likes']+x['comments']*10, reverse=True)
+
+        return res
+
+
+if __name__ == '__main__':
+    bot = VkTools(acces_token)
+    params = bot.get_profile_info(199494788)
+    users = bot.search_users(params)
+    pprint(users)
+    # pprint(bot.get_photos(users[2]['id']))
---
+++ data_store.py
@@ -0,0 +1,49 @@
+import sqlalchemy
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, Session, session, sessionmaker
+from sqlalchemy import create_engine, MetaData
+import psycopg2
+
+from config import db_url_object
+
+Base = declarative_base()
+engine = sqlalchemy.create_engine(db_url_object)
+metadata = MetaData()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Viewed(Base):
+    __tablename__ = 'viewed'
+    profile_id = sq.Column(sq.Integer, primary_key=True)
+    worksheet_id = sq.Column(sq.Integer, primary_key=True)
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+
+def add_bd(user_id, search_id):
+    '''Добавление в базу данных'''
+    to_bd = Viewed(profile_id=user_id, worksheet_id=search_id)
+    session.add(to_bd)
+    session.commit()
+
+def extraction_bd(user_id):
+    '''Извлечение из базы данных'''
+    ext_bd = list()
+    from_bd = session.query(Viewed).filter(Viewed.profile_id == user_id).all()
+    for item in from_bd:
+        ext_bd.append(item.worksheet_id)
+    return ext_bd
+
+def checked(user_id, user):
+    '''Проверка есть ли в базе данных пользователь'''
+    extraction_bd(user_id)
+    if user in extraction_bd(user_id):
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    create_tables(engine)
+    print(extraction_bd(199494788))
+    print(checked(199494788, 139520938))
+
